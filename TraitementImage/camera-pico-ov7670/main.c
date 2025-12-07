842de71dcaca934553056918eb826539486be924
Théophile D <tdeconche@laposte.net>
2025-12-07 22:22:34 +0100

    Librairie Camera

--- TraitementImage/camera-pico-ov7670/main.c
+++ TraitementImage/camera-pico-ov7670/main.c
@@ -1,114 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+
 #include "hardware/i2c.h"
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include "camera.h"
-// ! GP pins
-#define CAMERA_XCLK_PIN 21
-#define CAMERA_PCLK_PIN 10
-#define CAMERA_HREF_PIN 9
-#define CAMERA_RES_PIN 16
-#define CAMERA_PWDN_PIN 15
-#define CAMERA_VSYNC_PIN 22
-#define CAMERA_SDA      26
-#define CAMERA_SCL      27
-#define CAMERA_D0 14
-#define CAMERA_D1 17
-#define CAMERA_D2 13
-#define CAMERA_D3 18
-#define CAMERA_D4 12
-#define CAMERA_D5 19
-#define CAMERA_D6 11
-#define CAMERA_D7 20
+#include "init_camera.h"
 
-static inline int __i2c_write_blocking(void *i2c_handle, uint8_t addr, const uint8_t *src, size_t len)
+
+int main()
 {
-    return i2c_write_blocking((i2c_inst_t *)i2c_handle, addr, src, len, false);
-}
-
-static inline int __i2c_read_blocking(void *i2c_handle, uint8_t addr, uint8_t *dst, size_t len)
-{
-    return i2c_read_blocking((i2c_inst_t *)i2c_handle, addr, dst, len, false);
-}
-
-int main() {
     stdio_init_all();
     sleep_ms(2000);
 
-    // init I2C:
-    i2c_init(i2c1, 100000);
-    gpio_set_function(CAMERA_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(CAMERA_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(CAMERA_SDA);
-    gpio_pull_up(CAMERA_SCL);
-
-    // init RES:
-    gpio_init(CAMERA_RES_PIN);
-    gpio_set_dir(CAMERA_RES_PIN, GPIO_OUT);
-    gpio_put(CAMERA_RES_PIN, true);
-
-    // init PWDN:
-    gpio_init(CAMERA_PWDN_PIN);
-    gpio_set_dir(CAMERA_PWDN_PIN, GPIO_OUT);
-    gpio_put(CAMERA_PWDN_PIN, false);
-
-    // init sync pins
-    gpio_init(CAMERA_VSYNC_PIN);
-    gpio_set_dir(CAMERA_VSYNC_PIN, GPIO_IN);
-    gpio_init(CAMERA_HREF_PIN);
-    gpio_set_dir(CAMERA_HREF_PIN, GPIO_IN);
-    gpio_init(CAMERA_PCLK_PIN);
-    gpio_set_dir(CAMERA_PCLK_PIN, GPIO_IN);
-
-    // TODO : essayer bus aquisition parallele
-    // init DATA pins
-    int datapins[8] = {CAMERA_D0, CAMERA_D1, CAMERA_D2, CAMERA_D3, CAMERA_D4, CAMERA_D5, CAMERA_D6, CAMERA_D7};
-    for (int i=0; i<8; i++) {
-        gpio_init(datapins[i]);
-        gpio_set_dir(datapins[i], GPIO_IN);
-    }
-
-    // init camera:
     struct camera camera;
-    struct camera_platform_config platform = {
-        .i2c_write_blocking = __i2c_write_blocking,
-        .i2c_read_blocking = __i2c_read_blocking,
-        .i2c_handle = i2c1,
-        .xclk_pin = CAMERA_XCLK_PIN,
-        .xclk_divider = 15,  // 5 MHz, stable
-        .vsync_pin = CAMERA_VSYNC_PIN,
-        .href_pin = CAMERA_HREF_PIN,
-        .pclk_pin = CAMERA_PCLK_PIN,
-        .data_pins = {
-            CAMERA_D0, CAMERA_D1, CAMERA_D2, CAMERA_D3,
-            CAMERA_D4, CAMERA_D5, CAMERA_D6, CAMERA_D7,
-        },
-    };
 
-    // ? CHOIX FORMAT
-    const uint16_t width = CAMERA_WIDTH_DIV8;
-    const uint16_t height = CAMERA_HEIGHT_DIV8;
-    OV7670_size size = OV7670_SIZE_DIV8;
+    init_camera();
+    struct camera_platform_config platform = create_camera_platform_config();
 
-    printf("Camera to be initialized\n");
+    /* Choix Format */
+    uint16_t width_temp, height_temp;
+    OV7670_size size;
+    int division = 0; // 0 : DIV 8, 1 : DIV 4, ...
+    int format_out = choix_format(division, &width_temp, &height_temp, &size);
+    const uint16_t width = width_temp;
+    const uint16_t height = height_temp;
+
+    printf("Camera to be initialised\n");
     if (camera_init(&camera, &platform, size)) return 1;
-    printf("Camera initialized\n");
+    printf("Camera initialised\n");
 
-    uint8_t *frame_buffer = malloc(2 * width * height);
-    if (!frame_buffer) return 1;
+    /* Creation Buffers Camera */
+    uint8_t *frame_buffer, *outbuf;
+    int creation_buffer_out = creation_buffers_camera(&frame_buffer, &outbuf,
+                           width, height);
 
-    uint8_t *outbuf = malloc(width * height);   // 1 octet par pixel (P5)
-    if (!outbuf) {
-        free(frame_buffer);
-        return 1;
-    }
-
-    while (1) {
-
+    while (1)
+    {
         camera_capture_blocking(&camera, frame_buffer, width, height);
 
-        // Header P5
+        // Header P5 : format et dimensions de l'image
         printf("P5\n%d %d\n255\n", width, height);
 
         // Extraire Y seulement
